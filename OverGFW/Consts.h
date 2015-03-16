1890ea4083048853db44e8603fb21dc379474124
Joywii <kingzwt@gmail.com>
2015-03-16 13:27:48 +0800

    add youmi sdk

--- OverGFW/Consts.h
+++ OverGFW/Consts.h
@@ -12,4 +12,7 @@
 #define kScreenHeight         [UIScreen mainScreen].bounds.size.height
 #define kScreenWidth          [UIScreen mainScreen].bounds.size.width
 
+#define kYouMiDestribureID    @"024e3262df1bbe90"
+#define kYouMiSecret          @"d5e7984fc1939256"
+
 #endif
